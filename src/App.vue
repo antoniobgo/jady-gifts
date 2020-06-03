231e6999591655d9c22ea08cd53017339cb6777f
Antonio Gomes <antoniobmirandagomes@gmail.com>
2020-06-03 17:30:56 -0300

    list items working

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  v-app
+    v-app-bar(app)
+      v-row(justify="center")
+        v-toolbar-title Jadynha e seus presentes
+    v-content(fluid)
+      v-container
+        router-view
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+v-row(justify="center")
+  v-card(outlined max-width="400")
+    v-list(
+      v-if="giftList"
+      threeLine
+      )
+      template(v-for="(gift,index) in giftList")
+        v-list-item.my-1(:key="index")
+          v-list-item-avatar
+            v-img(:src="getImgLink(gift)")
+          v-list-item-content
+            v-list-item-title {{ gift.name }}
+            v-list-item-subtitle.my-1(v-if="gift.priority") Prioridade alta
+            v-list-item-subtitle.my-1(v-if="!gift.avaiable") Esse produto já foi comprado por alguém
+            v-list-item-subtitle.my-1(v-else) Produto disponivel para compra
+        v-row.mb-2(dense justify="center")
+          v-btn(
+            v-if="gift.avaiable"
+            @click="setAsBought(gift)"
+            :loading="loading"
+            )
+            | Marcar como comprado
+          v-btn(
+            v-else
+            @click="setAsAvaiable(gift)"
+            :loading="loading"
+            )
+            | Marcar como disponível
+
+        v-divider
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      giftList: undefined,
+      loading: false,
+    };
+  },
+  mounted() {
+    axios
+      .get("https://5ed52f3e8769250016e6338a.mockapi.io/presentes")
+      .then((response) => {
+        this.giftList = response.data;
+      });
+  },
+  computed: {},
+  methods: {
+    getImgLink(gift) {
+      return gift.imgLink;
+    },
+    setAsBought(gift) {
+      gift.avaiable = false;
+      this.loading = true;
+      axios
+        .put(
+          "https://5ed52f3e8769250016e6338a.mockapi.io/presentes/" + gift.id,
+          gift
+        )
+        .then(() => {
+          this.loading = false;
+        });
+    },
+    setAsAvaiable(gift) {
+      gift.avaiable = true;
+      this.loading = true;
+      axios
+        .put(
+          "https://5ed52f3e8769250016e6338a.mockapi.io/presentes/" + gift.id,
+          gift
+        )
+        .then(() => {
+          this.loading = false;
+        });
+    },
+  },
+};
 </script>
